49962d203a223b3735acdbff4505a272c1b6cf4c
DATBOI <alexsimons9999@gmail.com>
2018-05-06 17:27:45 -0500

    Where to start???

--- src/main/java/io/acari/streams/StreamDreams.java
+++ src/main/java/io/acari/streams/StreamDreams.java
@@ -1,10 +1,22 @@
 package io.acari.streams;
 
+import java.util.LinkedList;
 import java.util.stream.Stream;
 
 public class StreamDreams {
 
-  public static void main(String... args){
+  public static void main(String... args) {
 
+    //terminal operations
+    //allmatch, nonmatch, anymatch, count, findany min max
+
+    LinkedList<Integer> numbers = Stream.iterate(1, a -> ++a)
+        .limit(6)
+        .collect(
+            () -> new LinkedList<>(),//Supplies the collection to store results
+            (list, streamItem) -> list.add(streamItem),//mutates the collection
+            (listOne, listTwo) -> listOne.addAll(listTwo)//merges multiple data collections into one
+            //This will be covered in stream intervention
+        );
   }
 }
