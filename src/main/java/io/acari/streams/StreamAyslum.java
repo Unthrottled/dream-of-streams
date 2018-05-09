f6d737855d03f4cd0d151e6efdcaea52a81e7776
DATBOI <alexsimons9999@gmail.com>
2018-05-09 05:59:55 -0500

    That's not so bad

--- src/main/java/io/acari/streams/StreamAyslum.java
+++ src/main/java/io/acari/streams/StreamAyslum.java
@@ -1,9 +1,29 @@
 package io.acari.streams;
 
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class StreamAyslum {
 
   public static void main(String... args){
     //Nest collectors.
+    Map<Integer, Set<String>> loudSets = Stream.of("ao e aueta ustna ts naeua euae unaue sat uebsea uobtao eubtna ouebnhtae uotn".split(" "))
+        .collect(Collectors.groupingBy(String::length,
+            Collectors.mapping(String::toUpperCase, Collectors.toSet())));
+
+    System.out.println(loudSets);
+    System.out.println();
+
+    Map<Integer, Set<String>> loudSetsReadable = Stream.of("ao e aueta ustna ts naeua euae unaue sat uebsea uobtao eubtna ouebnhtae uotn".split(" "))
+        .map(String::toUpperCase)
+        .collect(Collectors.groupingBy(String::length, Collectors.toSet()));
+
+    System.out.println(loudSets);
+    System.out.println();
+
+
     //Concurrently
 
     //map-reduce all substrings for type ahead
