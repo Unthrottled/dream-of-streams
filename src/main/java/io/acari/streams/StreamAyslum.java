2f624aaccf7e36e67e0e821b398ffd5589193071
DATBOI <alexsimons9999@gmail.com>
2018-05-09 06:25:19 -0500

    parallelization is neat

--- src/main/java/io/acari/streams/StreamAyslum.java
+++ src/main/java/io/acari/streams/StreamAyslum.java
@@ -1,5 +1,9 @@
 package io.acari.streams;
 
+import com.google.common.collect.Lists;
+
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -25,6 +29,46 @@ public class StreamAyslum {
 
 
     //Concurrently
+//    This method does not actually reorder the elements; it just tells the JVM that if an
+//    order-based stream operation is applied, the order can be ignored.
+    StringBuilder twentyNumbers = Stream.iterate(1, a -> ++a)
+        .unordered()
+        .parallel()
+        .limit(20)//Put it here results are undefined
+        .map(number-> number + " ")
+        .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append);
+    System.out.println(twentyNumbers.toString());
+    System.out.println();
+
+
+    StringBuilder twentyNumbersAlwaysTheSame = Stream.iterate(1, a -> ++a)
+        .limit(20)//Put it here results are defined
+        .unordered()
+        .parallel()
+        .map(number-> number + " ")
+        .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append);
+    System.out.println(twentyNumbersAlwaysTheSame.toString());
+    System.out.println();
+
+    List<String> orderIsGreat = Lists.newArrayList("i cannot think of anything clever at the moment".split(" "))
+        .stream()
+        .collect(LinkedList::new, List::add, (listOne, listTwo) -> {
+          System.out.println("Joining forks!");//neverGetsCalled
+          listOne.addAll(listTwo);
+        });
+
+    System.out.println(orderIsGreat);
+    System.out.println();
+
+    List<String> thisShouldBeOrderedToo = Lists.newArrayList("i cannot think of anything clever at the moment".split(" "))
+        .parallelStream()
+        .collect(LinkedList::new, List::add, (listOne, listTwo) -> {
+          System.out.println("Joining forks!");
+          listOne.addAll(listTwo);
+        });
+
+    System.out.println(thisShouldBeOrderedToo);
+    System.out.println();
 
     //map-reduce all substrings for type ahead
     //create a multi-map.
