931f6de2b30b4c86b7c4e8dfbdb7066f0dc3bbf3
DATBOI <alexsimons9999@gmail.com>
2018-05-15 18:12:43 -0500

    these buggers did not make it in

---
+++ animations/src/app/stream/StreamItemContainer.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var StreamItemContainer = /** @class */ (function () {
+    function StreamItemContainer(_element) {
+        this._element = _element;
+        this._identifier = new Date().getTime();
+    }
+    Object.defineProperty(StreamItemContainer.prototype, "element", {
+        get: function () {
+            return this._element;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(StreamItemContainer.prototype, "identifier", {
+        get: function () {
+            return this._identifier;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    StreamItemContainer.prototype.toString = function () {
+        return this._identifier.toString();
+    };
+    return StreamItemContainer;
+}());
+exports.StreamItemContainer = StreamItemContainer;
+//# sourceMappingURL=StreamItemContainer.js.map
