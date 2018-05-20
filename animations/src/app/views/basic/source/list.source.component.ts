6c77fb32a4a06472ab90b2f5975b068651ac43e3
DATBOI <alexsimons9999@gmail.com>
2018-05-20 10:35:47 -0500

    moar list stream happy

--- animations/src/app/views/basic/source/list.source.component.ts
+++ animations/src/app/views/basic/source/list.source.component.ts
@@ -30,76 +30,20 @@ export class ListSourceComponent implements OnInit {
         this.list.element
             .map(el=>Observable.of(el))
             .map(element=>new SingleStreamItem(element))
-            .subscribe(item=> this.itemsToMoveAlong.push(item))
+            .subscribe(item=> this.itemsToMoveAlong.push(item), er=>{},
+                ()=> this.startStreamOne());
     }
-
-
-    mapOne: Function<StreamItem, StreamItem> = {
-        apply: (streamItem: StreamItem) => new SingleStreamItem(
-            streamItem.element.flatMap((element: Element) => this.hip2B.createStreamItem(()=>{
-                return {
-                        fill: element.options.get('fill'),
-                            stroke: element.options.get('stroke'),
-                    }
-                }).element
-            ))
-    };
-
-    flatMapOne: Function<StreamItem, Observable<StreamItem>> = {
-        apply: (streamItem: StreamItem) => Observable.create((observer: Observer<StreamItem>) => {
-            streamItem.element.subscribe((element: Element) => {
-                let triangle = ()=>
-                    this.triangleFactory.createStreamItem(()=>{
-                        return {
-                            fill: element.options.get('fill'),
-                                stroke: element.options.get('stroke'),
-                        }
-                    });
-                observer.next(triangle());
-                Observable.interval(750, Scheduler.async)
-                    .take(4)
-                    .subscribe(_ => observer.next(triangle()),
-                        observer.error,
-                        observer.complete);
-            });
-        })
-    };
-    filterOne: Predicate<StreamItem> = {
-        test: (item: StreamItem) => item.identifier % 2 === 0
-    };
-    private sourceOutputSubject = new BehaviorSubject(null);
-    sourceOutput = this.sourceOutputSubject.filter(item => !!item);
-
-    private mapSubject = new BehaviorSubject(null);
-    mapOutputStream = this.mapSubject.filter(item => !!item);
-
-    private flatMapSubject = new BehaviorSubject(null);
-    flatMapOutputStream = this.flatMapSubject.filter(item => !!item);
 
     private streamSourceInputSubject = new BehaviorSubject<StreamItem>(null);
     streamSourceInput = this.streamSourceInputSubject.filter(item => !!item);
 
-    constructor(private triangleFactory: TriangleStreamItemService,
-                private hip2B: SquareStreamItemService,
-                private circleService: CircleStreamItemService) {
+    constructor(private circleService: CircleStreamItemService) {
     }
 
     sourceComplete(item: StreamItem) {
-        this.sourceOutputSubject.next(item);
+        this.startStreamOne();
     }
 
-
-    flatMapOneComplete(steamItem: StreamItem) {
-        this.flatMapSubject.next(steamItem)
-    }
-
-    mapOneComplete(steamItem: StreamItem) {
-        this.mapSubject.next(steamItem)
-    }
-
-    filterOneComplete(steamItem: StreamItem) {
-
-    }
 
     private listIndex = -1;
     startStreamOne(): void {
