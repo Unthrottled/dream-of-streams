029ea1f369fe3757b02c6af21427e766fb03529d
DATBOI <alexsimons9999@gmail.com>
2018-05-23 06:19:06 -0500

    less 3 party ranbo

--- animations/src/app/stream/RanboShapeOptionsService.js
+++ animations/src/app/stream/RanboShapeOptionsService.js
@@ -1,11 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var randomColor = require('randomcolor');
 var RanboShapeOptionsService = /** @class */ (function () {
     function RanboShapeOptionsService() {
     }
     RanboShapeOptionsService.createStreamOption = function () {
-        var color = randomColor(new Date().getSeconds());
+        var color = 'red';
         return {
             stroke: { color: 'black', width: 2, opacity: 15 },
             fill: { color: color, opacity: 0.75 }
--- animations/src/app/stream/RanboShapeOptionsService.ts
+++ animations/src/app/stream/RanboShapeOptionsService.ts
@@ -1,11 +1,10 @@
 import {ShapeOptions} from "@progress/kendo-drawing";
 
-let randomColor = require('randomcolor');
 
 export class RanboShapeOptionsService {
 
     static createStreamOption(): ShapeOptions {
-        let color = randomColor(new Date().getSeconds());
+        let color = 'red';
         return {
             stroke: {color: 'black', width: 2, opacity: 15},
             fill: {color: color, opacity: 0.75}
