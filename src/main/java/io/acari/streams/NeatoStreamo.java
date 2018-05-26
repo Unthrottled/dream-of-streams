1f27ccde1590f4bc8fd856b0d93b46fbe98689ee
DATBOI <alexsimons9999@gmail.com>
2018-05-25 19:14:28 -0500

    The cool crew club, really nice that they chose not to use k

--- src/main/java/io/acari/streams/NeatoStreamo.java
+++ src/main/java/io/acari/streams/NeatoStreamo.java
@@ -1,10 +1,30 @@
 package io.acari.streams;
 
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class NeatoStreamo {
 
   public static void main(String... args){
+    Stream<String> theCoolCrew = Stream.of("Alex;Steve;Amy;Chad;Bill".split(";"));
+    theCoolCrew.forEach(System.out::println);
 
+    try {
+      theCoolCrew.forEach(stringToPrint -> System.out.println(stringToPrint));
+    } catch (IllegalStateException ignored) {
+      System.out.println("Only one terminal operatian can be applied to a" +
+          " stream, streams are not re-usable.");
+      System.out.println();
+
+
+      List<String> coolCrew = Stream.of("Alex;Steve;Amy;Chad;Bill".split(";"))
+          .collect(Collectors.toList());
+
+      System.out.println(coolCrew);
+
+
+    }
   }
 }
