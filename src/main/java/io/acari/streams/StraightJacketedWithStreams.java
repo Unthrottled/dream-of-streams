847b7ec9f843ab102ff5592382263e1c174e9650
DATBOI <alexsimons9999@gmail.com>
2018-05-26 15:36:07 -0500

    the numbers mason, what do the mean?

--- src/main/java/io/acari/streams/StraightJacketedWithStreams.java
+++ src/main/java/io/acari/streams/StraightJacketedWithStreams.java
@@ -68,11 +68,6 @@ public class StraightJacketedWithStreams {
             theNumbersMason.parallelStream().findAny().ifPresent(System.out::println)//JVM selects the first thread to finish the task and retrieves its data
         );
 
-
-
-
-    //START OF WITH NUMBERS
-
     //THEN COLLECTIONS
     System.out.println(Collectors.toList().characteristics());
     System.out.println(Collectors.toMap(Function.identity(), Function.identity())
@@ -82,6 +77,32 @@ public class StraightJacketedWithStreams {
         .characteristics());
     System.out.println(Collectors.groupingByConcurrent(Function.identity(), Collectors.toSet())
         .characteristics());
+
+
+//    start with just a parallel stream, then add the println, then introduce the orderd
+
+//    This method does not actually reorder the elements; it just tells the JVM that if an
+//    order-based stream operation is applied, the order can be ignored.
+
+    //move the limit after parallel
+
+    StringBuilder assimilation_is_eminent= Stream.iterate(1, a -> ++a)
+        .limit(20)
+        .unordered()
+        .parallel()
+        .map(number -> number + " ")
+        .collect(StringBuilder::new, StringBuilder::append, (stringBuilder, otherStringBuilder) -> {
+          System.out.println("assimilation is eminent");
+          stringBuilder.append(otherStringBuilder);
+        });
+
+    System.out.println(assimilation_is_eminent);
+
+    //    Any stream operation that is based on order, including findFirst() , limit() , or
+//    skip() , may actually perform more slowly in a parallel environment.
+//    This is a result of a parallel processing task being forced to coordinate all of its threads in a synchronized-like
+//    fashion.
+
 
 
   }
