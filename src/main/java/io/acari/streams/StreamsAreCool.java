f743b6eb1cbfd8d956c65088d71dbffb782934bf
Alex Simons <alexsimons9999@gmail.com>
2018-06-07 15:38:10 -0500

    ended here

--- src/main/java/io/acari/streams/StreamsAreCool.java
+++ src/main/java/io/acari/streams/StreamsAreCool.java
@@ -1,7 +1,9 @@
 package io.acari.streams;
 
 import com.google.common.base.Strings;
+import com.google.common.collect.LinkedListMultimap;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Multimap;
 import com.google.common.collect.Sets;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,10 +20,10 @@ public class StreamsAreCool {
 
         List<PodMember> podMembersEnhanced = Stream.of((
                 "Alex Java,Functional_Programming,Javascript,Fixing_bugs,Fun_Commit_Messages;" +
-                        "Steve Cobol,Cobol,Ada,Java,Groovy,Pineapples,Sunday_Mornings;" +
-                        "Amy Javascript,Javascript,HTML,CSS,Spiders,Assembly;" +
-                        "Chad PHP,PHP,Java,Ruby,Purple_Stuff,Anime;" +
-                        "Mira Python,Python,Ruby,Whitespaces,Java,Lombok,Curly_Brace_Placement").split(";"))
+                        "Steve Cobol,Ada,Java,Groovy,Pineapples,Sunday_Mornings;" +
+                        "Amy Javascript,HTML,CSS,Spiders,Assembly;" +
+                        "Chad PHP,Java,Ruby,Purple_Stuff,Anime;" +
+                        "Mira Python,Ruby,Whitespaces,Java,Lombok,Curly_Brace_Placement").split(";"))
                 .map(podMemberInfo -> {
                     String[] split = podMemberInfo.split(" ");
                     return PodMember.builder()
@@ -42,6 +44,60 @@ public class StreamsAreCool {
 
         Pod pod = podMemberStream.collect(Pod::new, Pod::addPodMember, Pod::combinePod);
 
+
+        pod.addPodMember(PodMember.builder()
+                .name("Alex")
+                .interests(new Interests(Lists.newArrayList("Imitation", "Not being real")))
+        .build());
+
+
+        Map<String, List<PodMember>> byInterest = pod.fetchPodMembers()
+                .distinct()
+                .flatMap(podMember -> podMember.getInterests().interestsAsStream().map(interest -> new AbstractMap.SimpleEntry<>(interest, podMember)))
+                .flatMap(stringPodMemberSimpleEntry -> {
+                    Stream.Builder<AbstractMap.SimpleEntry<String, PodMember>> bob = Stream.builder();
+                    String interest = stringPodMemberSimpleEntry.getKey();
+                    for (int i = 0; i < interest.length(); i++) {
+                        for (int j = i + 1 ; j <= interest.length(); j++) {
+                            bob.accept(new AbstractMap.SimpleEntry<>(interest.substring(i,j),
+                                    stringPodMemberSimpleEntry.getValue()));
+                        }
+                    }
+                    return bob.build().parallel();
+                })
+                .collect(Collectors.groupingBy(Map.Entry::getKey,
+                        Collectors.mapping(Map.Entry::getValue, Collectors.toList())));
+
+
+        List<String> sortedInterests = pod.fetchPodMembers().map(PodMember::getInterests)
+                .flatMap(Interests::interestsAsStream)
+                .sorted()
+                .collect(Collectors.toList());
+
+        System.out.println(sortedInterests);
+
+//        - The identity must be defined such that for all elements in the stream u ,
+//        - combiner.apply(identity, u) is equal to u .
+//
+//        - The accumulator operator op must be associative and stateless such that (a op b) op c
+//        - is equal to a op (b op c) . (1 + 2) + 3 == 1 + (2 + 3)
+//
+//        - The combiner operator must also be associative and stateless and compatible with the
+//        - identity, such that for all u and t combiner.apply(u,accumulator.apply(identity,t))
+//                - is equal to accumulator.apply(u,t) .
+
+        List<String> alsoInterests = sortedInterests.stream()
+                .unordered()
+                .parallel()
+                .collect(() -> {
+                    System.out.println("New Identity");
+                    return new LinkedList<String>();
+                }, LinkedList::add, (strings, c) -> {
+                    System.out.println("Joining fork results");
+                    strings.addAll(c);
+                });
+
+        System.out.println(alsoInterests);
 
 
         System.out.println("Done");
@@ -97,5 +153,9 @@ class Interests {
         return coreInterests.stream()
                 .map(String::toLowerCase)
                 .anyMatch(interest-> interest.contains(java));
+    }
+
+    public Stream<String> interestsAsStream(){
+        return coreInterests.stream();
     }
 }
